99e89776a2bcc1159b9d8e216a8ea65357c96c23
Aryan <aryaninmail@gmail.com>
2023-12-14 18:20:43 +0530

    webhook env added

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -6,9 +6,7 @@ import { NextResponse } from "next/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 
 export async function POST(req: Request) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  //   TODO: Add webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
     throw new Error(
